979ba9b7ec26d54f543c10d5c81f0c57ed00145a
nite48 <nite48ru@gmail.com>
2022-02-04 11:21:40 +0300

    Create models

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { isEmail } = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator(v) {
+        return isEmail(v);
+      },
+      message: 'Некорректный Email',
+    },
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password,
+) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error('Неправильные почта или пароль'));
+        }
+        return user;
+      });
+    });
+};
+module.exports = mongoose.model('user', userSchema);
